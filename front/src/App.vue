c8e2a7390c4b90142ea2ff9a49394ac7cb46c82a
Anessi Boka <k.yao1305@gmail.com>
2024-11-13 15:16:39 +0000

    mise à jour curseur

--- front/src/App.vue
+++ front/src/App.vue
@@ -7,5 +7,27 @@
 <script>
 export default {
   name: 'App',
+  mounted() {
+    // Créez l'élément de curseur personnalisé
+    const cursor = document.createElement("div");
+    cursor.classList.add("custom-cursor");
+    document.body.appendChild(cursor);
+
+    // Suivez le mouvement de la souris
+    document.addEventListener("mousemove", (e) => {
+      cursor.style.left = `${e.clientX}px`;
+      cursor.style.top = `${e.clientY}px`;
+    });
+
+    // Ajoutez un effet de loupe au survol des éléments avec la classe .hover-effect
+    document.querySelectorAll(".hover-effect").forEach((element) => {
+      element.addEventListener("mouseover", () => {
+        element.classList.add("zoomed-element"); // Ajoute l'effet de loupe
+      });
+      element.addEventListener("mouseout", () => {
+        element.classList.remove("zoomed-element"); // Retire l'effet de loupe
+      });
+    });
+  },
 };
 </script>
