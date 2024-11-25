23651d2c934a69ad1734527b8819d981271d37f3
Anessi Boka <k.yao1305@gmail.com>
2024-11-25 14:07:23 +0000

    Mise à jour

--- front/src/components/LandingPage.vue
+++ front/src/components/LandingPage.vue
@@ -154,7 +154,7 @@
                         <div class="project-thumbnail w-[150px] h-[150px] mb-4 mt-4">
                             <img src="/b.png" alt="Miniature du projet" class="object-cover w-auto h-auto rounded-lg">
                         </div>
-                        <div class="experience-details ml-6 flex flex-col">
+                        <div class="experience-details ml-4 flex flex-col">
                             <h1 class="text-lg font-semibold">Pain de Vie · Projet personnel</h1>
                             <p class="text-base text-gray text-justify">
                                 Application mobile pour le partage quotidien de Versets Bibliques.
