78b41a3a1b4728ab9dab5e2038bb81f47bbcac0a
Anessi Boka <k.yao1305@gmail.com>
2024-11-25 15:50:26 +0000

    Mise à jour

--- front/src/components/LandingPage.vue
+++ front/src/components/LandingPage.vue
@@ -180,7 +180,7 @@
 
                     <!-- Projet 1 -->
                     <div class="experience-item flex flex-row mt-5 text-custom-green font-inter">
-                        <div class="project-thumbnail w-[200px] h-[200px] mb-4 mt-5 ">
+                        <div class="project-thumbnail w-[100px] h-[100px] mb-4 mt-5 ">
                             <img src="/bugster.png" alt="Miniature du projet" class="object-fit">
                         </div>
                         <div class="experience-details flex flex-col">
