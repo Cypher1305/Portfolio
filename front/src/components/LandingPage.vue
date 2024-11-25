abc09e21d4fd7457fff284a0d658eafe3b6efac7
Anessi Boka <k.yao1305@gmail.com>
2024-11-25 16:13:57 +0000

    Mise à jour

--- front/src/components/LandingPage.vue
+++ front/src/components/LandingPage.vue
@@ -14,7 +14,7 @@
                 <img :src="profileImage" alt="Photo" class="rounded-full w-40 h-30 mt-1 photo" />
                 <div class="w-[80%] mx-auto flex flex-col ">
                     <h1 class="text-2xl text-white font-inter font-bold mt-2 text-left">YAO Kossia Bénédicte</h1>
-                    <h4 class="text-2xl text-white font-inter text-left">Développeur Full Stack</h4>
+                    <h4 class="text-1xl text-white font-inter text-left">Développeur Full Stack</h4>
                     <p class="text-base text-gray font-inter mb-2 text-center text-justify">
                         Je conçois des sites et des applications web/mobiles, ainsi que des projets pour répondre aux
                         défis actuels.
